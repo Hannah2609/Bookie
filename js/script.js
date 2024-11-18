3f6b1839e6ec985588d81a3fc233e0180fceae0f
Hannah2609 <hann7419@stud.kea.dk>
2024-11-18 16:06:51 +0100

    .

--- js/script.js
+++ js/script.js
@@ -2,6 +2,18 @@ function toggleMenu() {
     const navLinks = document.querySelector(".nav-links");
     const burgerMenu = document.querySelector(".burger-menu"); 
 
+    const loggedInUser = sessionStorage.getItem("user_id");
+    const logInBtn = document.querySelector("#loginBtn")
+    const profileBtn = document.querySelector("#profileBtn");
+    const adminBtn = document.querySelector("#adminBtn");
+
+
     navLinks.classList.toggle("show");
     burgerMenu.classList.toggle("close");
 }
+
+function signOut() {
+    console.log("signOut")
+}
+
+
