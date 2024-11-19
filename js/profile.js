db548d9315db0030b339619eefd844ef4daed14c
Hannah2609 <hann7419@stud.kea.dk>
2024-11-19 12:25:03 +0100

    profile

--- js/profile.js
+++ js/profile.js
@@ -1,12 +1,13 @@
 import { baseUrl, handleAPIResponseError } from "./common.js";
 
-const profileInfo = document.querySelector(".profile_info");
+const profile = document.querySelector(".profile_info");
+let profileInfo = {};
 
 document.addEventListener("DOMContentLoaded", showProfile)
 
 function showProfile() {
     let userId = sessionStorage.getItem("user_id");
-    console.log(`profile id ${userId}`);
+    console.log(`User id ${userId}`);
 
     if (!userId) {
         window.location.href = "login.html";
@@ -16,24 +17,71 @@ function showProfile() {
             .then((response) => response.json())
             .then((data) => {
                 if (Object.keys(data).includes("first_name")) {
-                    profileInfo.innerHTML = `
+                    profile.innerHTML = `
                     <h2>Hi, ${data.first_name} ${data.last_name}</h2>
                     <p>Bookie member since: ${data.membership_date}</p>
                     <h3>Your information</h3>
                     <div id="form">
                         <form id="profile-form">
-                            <input id="editEmail" type="email" placeholder="${data.email}">
-                            <input id="editPhoneNumber" type="text" placeholder="${data.phone_number}">
-                            <input id="editAddress" type="text" placeholder="${data.address}">
-                            <input id="editBirth" type="text" placeholder="${data.birth_date}">
-                            <button class="filled-btn" type="submit">Save changes</button>
+                            <input id="editEmail" type="email" value="${data.email}">
+                            <input id="editPhoneNumber" type="text" value="${data.phone_number}">
+                            <input id="editAddress" type="text" value="${data.address}">
+                            <input id="editBirth" type="text" value="${data.birth_date}">
+                            <button id="saveBtn" class="filled-btn" type="submit">Save changes</button>
                         </form>
                     </div>
-
                     `;
+                    profileInfo = data;
+                    document
+                        .querySelector("#profile-form")
+                        .addEventListener("submit", (e) => {
+                            e.preventDefault();
+                            updateProfile(userId);
+                        });
                 } else {
                     handleAPIResponseError(data.error);
                 }
         }).catch(handleAPIResponseError);
-
 }}; 
+
+function updateProfile(userId) {
+    const currentProfileInfo = {
+        email: document.querySelector("#editEmail").value.trim(),
+        phone_number: document.querySelector("#editPhoneNumber").value.trim(),
+        address: document.querySelector("#editAddress").value.trim(),
+        birth_date: document.querySelector("#editBirth").value.trim()
+    };
+
+    const params = new URLSearchParams();
+
+    // comparing current info with original and setting it in updated info - only the changed inputs
+    const updatedProfileInfo = {};
+    for (let key in currentProfileInfo) {
+        if (currentProfileInfo[key] !== profileInfo[key]) {
+            updatedProfileInfo[key] = currentProfileInfo[key];
+            params.append(key, updatedProfileInfo[key]);
+            // console.log(updatedProfileInfo);
+        }
+    }
+
+    // sennds message if trying to save changes without any made - TODO: maybe dissable btn if this
+    if (Object.keys(updatedProfileInfo).length === 0) {
+        alert("No changes made");
+        return;
+    }
+
+    fetch(`${baseUrl}/users/${userId}`, {
+        method: "PUT",
+        body: params,
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            if (data.status === "ok") {
+                alert("Update was successful");
+            } else {
+                handleAPIResponseError(data.error);
+                console.log("Request body:", JSON.stringify(updatedProfileInfo));
+            }
+        })
+        .catch(handleAPIResponseError);
+};
